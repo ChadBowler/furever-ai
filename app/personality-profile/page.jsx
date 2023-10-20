cd8a551e5297cc0b2708686fe68ba086f67315af
Chad Bowler <chadbowler@gmail.com>
2023-10-20 14:58:53 -0600

    adjusted answers for questions in personality profile.

--- app/personality-profile/page.jsx
+++ app/personality-profile/page.jsx
@@ -1,30 +1,48 @@
 // pages/PetQuestionnaire.js
 'use client';
 import { useState } from 'react';
-import questions from '../api/data.json'
+import questions from '../api/data.json';
 import Button from '../components/Button';
 
-function PetQuestionnaire() {
+const timeOptions = [
+  '0 hours',
+  '1-3 hours',
+  '4-6 hours',
+  '7-9 hours',
+  '10-12 hours',
+  '13-15 hours',
+  '16-18 hours',
+];
 
+const dwellingOptions = [
+  'Apartment',
+  'House',
+  'Farmhouse',
+  'Condo',
+  'Townhouse',
+  'Other',
+];
+
+function PetQuestionnaire() {
   const [answers, setAnswers] = useState({});
-  
+
   const handleInputChange = (e) => {
     const { name, value, type, checked } = e.target;
     setAnswers((prevAnswers) => ({
       ...prevAnswers,
-      [name]: type === "checkbox" ? checked : value,
+      [name]: type === 'checkbox' ? checked : value,
     }));
   };
 
   const handleSaveAnswers = () => {
     // Code to save answers to user's profile in a user database goes here
-    alert("Answers saved successfully!");
+    alert('Answers saved successfully!');
   };
 
   const handleSubmitAnswers = () => {
     // Code to collect answers and form a prompt for LLM goes here
     // You can use the 'answers' state to construct the prompt
-    alert("Answers submitted successfully!");
+    alert('Answers submitted successfully!');
   };
 
   return (
@@ -33,8 +51,25 @@ function PetQuestionnaire() {
       <form>
         {questions.map((q) => (
           <div key={q.question} className="question">
-            <label htmlFor={q.question}>{q.question}</label><br />
-            {q.type === "open ended" ? (
+            <label htmlFor={q.question}>{q.question}</label>
+            <br />
+            {q.question === 'How much time do you have to spend with a pet each day?' ? (
+              <select name={q.question} onChange={handleInputChange} required>
+                {timeOptions.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            ) : q.question === 'What type of dwelling do you live in?' ? (
+              <select name={q.question} onChange={handleInputChange} required>
+                {dwellingOptions.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            ) : q.type === 'open ended' ? (
               <textarea
                 id={q.question}
                 name={q.question}
@@ -67,10 +102,8 @@ function PetQuestionnaire() {
             )}
           </div>
         ))}
-        <Button onClick={handleSaveAnswers} buttonName={'Save Answers'}></Button>
-        <Button onClick={handleSubmitAnswers} buttonName={'Submit Answers'}></Button>
-        {/* <button type="button" onClick={handleSaveAnswers}>Save Answers</button>
-        <button type="button" onClick={handleSubmitAnswers}>Submit Answers</button> */}
+        <Button onClick={handleSaveAnswers} buttonName={'Save Answers'} />
+        <Button onClick={handleSubmitAnswers} buttonName={'Submit Answers'} />
       </form>
     </div>
   );
